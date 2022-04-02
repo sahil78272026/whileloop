de76ba53f244f37ee7f556edeab52ae5034fc29f
sahil78272026 <sahil.garg78272026@gmail.com>
2022-04-02 08:41:08 +0530

    Add files via upload

---
+++ 01_while_loop.py
@@ -0,0 +1,8 @@
+# loops are used when you want to do some repetetive task
+i = 50
+
+while i < 60:
+    print("Value of i is " , i)
+    i=i+1
+
+print("Now value of i is greater than or equal to 60")
